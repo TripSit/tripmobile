da20581928962296969984c66ac2e876388c16d6
Eddie Curtis <e.j.curtis1@gmail.com>
2014-11-29 17:14:31 +0000

    Removing bin directory from committed files

--- gen/me/tripsit/mobile/R.java
+++ gen/me/tripsit/mobile/R.java
@@ -973,6 +973,7 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int ic_launcher=0x7f020057;
+        public static final int logo=0x7f020058;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -989,6 +990,10 @@ containing a value of this type.
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int btn_factSheets=0x7f050041;
+        public static final int btn_generalChat=0x7f050040;
+        public static final int btn_tripsitChannel=0x7f05003f;
+        public static final int btn_wiki=0x7f050042;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1004,6 +1009,9 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int img_Logo=0x7f05003d;
+        public static final int layout_menu=0x7f05003c;
+        public static final int layout_menuButtons=0x7f05003e;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
@@ -1130,7 +1138,11 @@ containing a value of this type.
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int app_name=0x7f0a000d;
-        public static final int hello_world=0x7f0a000e;
+        public static final int btn_factsheets=0x7f0a0010;
+        public static final int btn_generalChat=0x7f0a000f;
+        public static final int btn_tripsitChannel=0x7f0a000e;
+        public static final int btn_wiki=0x7f0a0011;
+        public static final int str_tripsitLogo=0x7f0a0012;
     }
     public static final class style {
         /** 
