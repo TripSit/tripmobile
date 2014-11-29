3a94a70bb712d21a6ed90e0b556a99210d0f9f64
Eddie Curtis <e.j.curtis1@gmail.com>
2014-11-29 20:02:37 +0000

    Split layout into separate xml files
    This should let us load the header of multiple activities from the same
    layout file.

--- src/me/tripsit/mobile/Menu.java
+++ src/me/tripsit/mobile/Menu.java
@@ -2,17 +2,37 @@ package me.tripsit.mobile;
 
 import me.tripsit.mobile.chat.Constants;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class Menu extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_menu);
+		
+		LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		RelativeLayout head = (RelativeLayout) inflater.inflate(R.layout.main_layout, null);
+		LinearLayout content = (LinearLayout) inflater.inflate(R.layout.activity_menu, null);
+		
+		LinearLayout baseLayout = new LinearLayout(this);
+		baseLayout.setOrientation(LinearLayout.VERTICAL);
+		baseLayout.addView(head);
+		
+		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+				LinearLayout.LayoutParams.WRAP_CONTENT,
+				LinearLayout.LayoutParams.WRAP_CONTENT);
+		params.gravity = Gravity.CENTER_HORIZONTAL;
+		baseLayout.addView(content, params);
+		
+		setContentView(baseLayout);
 	}
 	
 	public void clickTripsit(View view) {
