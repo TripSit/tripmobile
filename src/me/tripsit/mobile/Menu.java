0b6cbcdd3cb3b2bcd5b41499c8fea0b706c3db26
Eddie Curtis <e.j.curtis1@gmail.com>
2014-11-29 20:58:28 +0000

    Added layout for factsheets and some refactoring

--- src/me/tripsit/mobile/Menu.java
+++ src/me/tripsit/mobile/Menu.java
@@ -1,38 +1,23 @@
 package me.tripsit.mobile;
 
+import me.tripsit.mobile.builders.LayoutBuilder;
 import me.tripsit.mobile.chat.Constants;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 
+/**
+ * The main menu activity which is shown to the user on startup
+ * @author Eddie Curtis
+ */
 public class Menu extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		RelativeLayout head = (RelativeLayout) inflater.inflate(R.layout.main_layout, null);
-		LinearLayout content = (LinearLayout) inflater.inflate(R.layout.activity_menu, null);
-		
-		LinearLayout baseLayout = new LinearLayout(this);
-		baseLayout.setOrientation(LinearLayout.VERTICAL);
-		baseLayout.addView(head);
-		
-		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-				LinearLayout.LayoutParams.WRAP_CONTENT,
-				LinearLayout.LayoutParams.WRAP_CONTENT);
-		params.gravity = Gravity.CENTER_HORIZONTAL;
-		baseLayout.addView(content, params);
-		
-		setContentView(baseLayout);
+		setContentView(LayoutBuilder.buildLinearLayout(this, R.layout.activity_menu, LayoutBuilder.buildParamsLinearCenterHorizontal()));
 	}
 	
 	public void clickTripsit(View view) {
@@ -41,5 +26,10 @@ public class Menu extends Activity {
 	
 	public void clickGeneralChat(View view) {
 		startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.GENERAL_URL)));
+	}
+	
+	public void clickFactsheets(View view) {
+		Intent intent = new Intent(this, Factsheets.class);
+		startActivity(intent);
 	}
 }
---
+++ src/me/tripsit/mobile/builders/LayoutBuilder.java
@@ -0,0 +1,39 @@
+package me.tripsit.mobile.builders;
+
+import me.tripsit.mobile.R;
+import android.app.Activity;
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+public class LayoutBuilder {
+	
+	public static LinearLayout buildLinearLayout(Activity origin, int viewId, LayoutParams params) {
+		LayoutInflater inflater = (LayoutInflater) origin.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		
+		LinearLayout baseLayout = new LinearLayout(origin);
+		baseLayout.setOrientation(LinearLayout.VERTICAL);
+		
+		baseLayout.addView((RelativeLayout) inflater.inflate(R.layout.main_layout, null));
+		
+		baseLayout.addView(inflater.inflate(viewId, null), params);
+		return baseLayout;
+	}
+
+
+	public static LinearLayout.LayoutParams buildParams() {
+		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+				LinearLayout.LayoutParams.WRAP_CONTENT,
+				LinearLayout.LayoutParams.WRAP_CONTENT);
+		return params;
+	}
+	
+	public static LayoutParams buildParamsLinearCenterHorizontal() {
+		LinearLayout.LayoutParams params = buildParams();
+		params.gravity = Gravity.CENTER_HORIZONTAL;
+		return params;
+	}
+}
