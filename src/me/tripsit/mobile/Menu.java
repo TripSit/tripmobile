ac05446da9532310092ea08dd0e5fd97244e4477
Eddie Curtis <e.j.curtis1@gmail.com>
2014-11-29 13:37:08 +0000

    Initial commit. Let's get this show on the road

---
+++ src/me/tripsit/mobile/Menu.java
@@ -0,0 +1,13 @@
+package me.tripsit.mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Menu extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_menu);
+	}
+}
