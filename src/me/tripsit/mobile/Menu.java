224c7acb2b04d99ea71f6cdd8e277dadfc1fa76d
Eddie Curtis <e.j.curtis1@gmail.com>
2014-11-29 18:29:07 +0000

    Added links to chat channels

--- src/me/tripsit/mobile/Menu.java
+++ src/me/tripsit/mobile/Menu.java
@@ -1,7 +1,11 @@
 package me.tripsit.mobile;
 
+import me.tripsit.mobile.chat.Constants;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class Menu extends Activity {
 
@@ -9,5 +13,13 @@ public class Menu extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_menu);
+	}
+	
+	public void clickTripsit(View view) {
+		startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.TRIPSIT_URL)));
+	}
+	
+	public void clickGeneralChat(View view) {
+		startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.GENERAL_URL)));
 	}
 }
---
+++ src/me/tripsit/mobile/chat/Constants.java
@@ -0,0 +1,19 @@
+package me.tripsit.mobile.chat;
+
+/**
+ * Constants used for the chat features
+ * @author Eddie Curtis
+ */
+public class Constants {
+
+	private static final String URL_FORMAT = "%s/?nick=%s?%s";
+	private static final String URL_BASE = "http://chat.tripsit.me";
+	
+	private static final String NICK = "TripMobileUser";
+	
+	private static final String TRIPSIT_CHAN = "#tripsit";
+	private static final String[] GENERAL_CHANNELS = new String[] {"#home", "#drugs"};
+	
+	public static final String TRIPSIT_URL = String.format(URL_FORMAT, URL_BASE, NICK, TRIPSIT_CHAN);
+	public static final String GENERAL_URL = String.format(URL_FORMAT, URL_BASE, NICK, android.text.TextUtils.join(",", GENERAL_CHANNELS));
+}
