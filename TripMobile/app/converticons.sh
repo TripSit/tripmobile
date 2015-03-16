8d8b993a58486967295352987b1d41693b8a425a
Eddie Curtis <e.j.curtis1@gmail.com>
2015-03-16 19:20:08 +0000

    Compiled social media icons

--- TripMobile/app/converticons.sh
+++ TripMobile/app/converticons.sh
@@ -26,14 +26,20 @@ SIZE=80
 ./pngconvert.sh -w$SIZE trippy_chat.svg
 ./pngconvert.sh -w$SIZE trippy_document_reverse.svg
 ./pngconvert.sh -w$SIZE trippy_document.svg
+./pngconvert.sh -w$SIZE trippy_facebook_reverse.svg
+./pngconvert.sh -w$SIZE trippy_facebook.svg
 ./pngconvert.sh -w$SIZE trippy_flasks_reverse.svg
 ./pngconvert.sh -w$SIZE trippy_flasks.svg
 ./pngconvert.sh -w$SIZE trippy_gears_reverse.svg
 ./pngconvert.sh -w$SIZE trippy_gears.svg
 ./pngconvert.sh -w$SIZE trippy_globe_reverse.svg
 ./pngconvert.sh -w$SIZE trippy_globe.svg
+./pngconvert.sh -w$SIZE trippy_reddit_reverse.svg
+./pngconvert.sh -w$SIZE trippy_reddit.svg
 ./pngconvert.sh -w$SIZE trippy_scale_reverse.svg
 ./pngconvert.sh -w$SIZE trippy_scale.svg
 ./pngconvert.sh -w$SIZE trippy_ts_reverse.svg
 ./pngconvert.sh -w$SIZE trippy_ts.svg
+./pngconvert.sh -w$SIZE trippy_twitter_reverse.svg
+./pngconvert.sh -w$SIZE trippy_twitter.svg
 
