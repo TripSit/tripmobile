aef318f2cf67d279cbd5253e074c8fd7aa2f9e27
Eddie Curtis <e.j.curtis1@gmail.com>
2015-03-16 19:26:45 +0000

    Removed plain icon pngs

--- TripMobile/app/converticons.sh
+++ TripMobile/app/converticons.sh
@@ -1,21 +1,5 @@
 #!/bin/bash -e
 SIZE=80
-./pngconvert.sh -w$SIZE plain_book_reverse.svg
-./pngconvert.sh -w$SIZE plain_book.svg
-./pngconvert.sh -w$SIZE plain_chat_reversel.svg
-./pngconvert.sh -w$SIZE plain_chat.svg
-./pngconvert.sh -w$SIZE plain_document_reverse.svg
-./pngconvert.sh -w$SIZE plain_document.svg
-./pngconvert.sh -w$SIZE plain_flasks_reverse.svg
-./pngconvert.sh -w$SIZE plain_flasks.svg
-./pngconvert.sh -w$SIZE plain_gears_reverse.svg
-./pngconvert.sh -w$SIZE plain_gears.svg
-./pngconvert.sh -w$SIZE plain_globe_reverse.svg
-./pngconvert.sh -w$SIZE plain_globe.svg
-./pngconvert.sh -w$SIZE plain_scale_reverse.svg
-./pngconvert.sh -w$SIZE plain_scale.svg
-./pngconvert.sh -w$SIZE plain_ts_reverse.svg
-./pngconvert.sh -w$SIZE plain_ts.svg
 ./pngconvert.sh -w$SIZE trippy_about.svg
 ./pngconvert.sh -w$SIZE trippy_about_reverse.svg
 ./pngconvert.sh -w$SIZE trippy_book_reverse.svg
