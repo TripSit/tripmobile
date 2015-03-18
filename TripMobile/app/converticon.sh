dad82b3db52e551d82f52041375f7969b9a7ee01
Eddie Curtis <e.j.curtis1@gmail.com>
2015-03-18 20:45:44 +0000

    Added new icon

--- TripMobile/app/converticon.sh
+++ TripMobile/app/converticon.sh
@@ -1,1 +1,3 @@
-inkscape -h512 --export-background-opacity=0 --export-png=src/main/res/drawable-xxxhdpi/icon.png src/main/svg/trippy_ts.svg
+inkscape -h512 --export-background-opacity=0 --export-png=src/main/res/drawable-xxxhdpi/icon.png src/main/svg/icon.svg
+HEIGHT=80
+./pngconvert.sh -h$HEIGHT icon.svg
