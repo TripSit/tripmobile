9240c19f4fd6cc4fa762a92ed092579b8a9b3f3d
EddieCurtis <e.j.curtis1@gmail.com>
2015-03-31 17:22:49 -0400

    Added release script

---
+++ release/release.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+# --- Release Script ---
+# This script is used to build a release version of the app
+#
+# Usage:
+# release.sh major (update major version)
+# release.sh minor (update minor version)
+# release.sh build (update build version)
+# release.sh       (release but keep current version)
+#
+
+IFS='*' # Change the IFS to prevent bash removing whitespace from variables
+ 
+GRADLE_LOCATION=../TripMobile/app/build.gradle
+
+version_line=$(grep versionCode $GRADLE_LOCATION)
+full_version=$(echo $version_line | awk '{print $2}')
+major=$(echo $full_version | sed 's/\..*//')
+minor=$(echo $full_version | sed 's/^[0-9]*\.//' | sed 's/\.[0-9]*$//')
+build=$(echo $full_version | sed 's/[0-9]*\.[0-9]*\.//')
+
+if [ $1 = 'major' ]
+  then
+    ((++major))
+    minor=0
+    build=0
+elif [ $1 = 'minor' ]
+  then
+    ((++minor))
+    build=0
+elif [ $1 = 'build' ]
+  then
+    ((++build))
+fi
+
+new_line=$(echo $version_line | sed "s/$full_version/$major.$minor.$build/")
+sed -i "s/$version_line/$new_line/" $GRADLE_LOCATION
+
+unset IFS # Unset the temp value we stored in IFS
